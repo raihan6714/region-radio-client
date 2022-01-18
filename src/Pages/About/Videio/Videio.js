78c4c73d9b3e9e2296c74a8a197a0b5f0b29a75a
raihan6714 <ab40096@gmail.com>
2022-01-18 23:14:21 +0600

    about complate

---
+++ src/Pages/About/Videio/Videio.js
@@ -0,0 +1,349 @@
+import React from "react";
+import sign3 from "../../../images/sign3.png";
+import video01 from "../../../images/video01.jpg";
+import team01 from "../../../images/team01.jpg";
+import team02 from "../../../images/team02.jpg";
+import team03 from "../../../images/team03.jpg";
+import team04 from "../../../images/team04.jpg";
+import team05 from "../../../images/team05.jpg";
+import team06 from "../../../images/team06.jpg";
+import partner01 from "../../../images/partner01.png";
+import partner02 from "../../../images/partner02.png";
+import partner03 from "../../../images/partner03.png";
+import partner04 from "../../../images/partner04.png";
+const Videio = () => {
+  return (
+    <div>
+      <div id="content" class="page-section">
+        <div class="container">
+          <div class="title1 right">
+            . <span>about us</span> .
+          </div>
+
+          <p class="text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+            fringilla non nunc vitae lobortis. Donec scelerisque varius
+            tincidunt. Aenean laoreet, odio id pulvinar mattis, quam dolor
+            ullamcorper urna, ut efficitur nulla purus in felis. Praesent leo
+            tellus, lacinia vel semper vel, molestie eget nisi. Suspendisse orci
+            libero, dictum eget convallis a, tempor a leo. Ut ligula ante,
+            fringilla feugiat est id, condimentum ornare est. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Morbi quis iaculis sem, quis pellentesque tellus.
+            Sed auctor purus eget nisi lacinia, eget tincidunt erat
+            sollicitudin. Donec eu ultricies nisl, sit amet ultrices justo.
+            Proin maximus magna purus, ut imperdiet mauris tempus sed. Nunc
+            eleifend mattis justo sit amet varius.
+          </p>
+
+          <p class="text-center my-4 my-md-5">
+            <img src={sign3} alt="" class="img-fluid" />
+          </p>
+
+          <div class="row">
+            <div class="col-md-3">
+              <div class="info1">
+                <div class="txt1">
+                  <span
+                    class="animated-number"
+                    data-duration="2000"
+                    data-animation-delay="0"
+                  >
+                    90
+                  </span>
+                  +
+                </div>
+                <div class="txt2">Projects Completed</div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="info1">
+                <div class="txt1">
+                  <span
+                    class="animated-number"
+                    data-duration="2000"
+                    data-animation-delay="0"
+                  >
+                    120
+                  </span>
+                  +
+                </div>
+                <div class="txt2">Happy clients</div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="info1">
+                <div class="txt1">
+                  <span
+                    class="animated-number"
+                    data-duration="2000"
+                    data-animation-delay="0"
+                  >
+                    50
+                  </span>
+                  +
+                </div>
+                <div class="txt2">WEB awards</div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="info1">
+                <div class="txt1">
+                  <span
+                    class="animated-number"
+                    data-duration="2000"
+                    data-animation-delay="0"
+                  >
+                    240
+                  </span>
+                  +
+                </div>
+                <div class="txt2">Cups of coffee</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="video1 mt-4 mt-md-5">
+            <a
+              href="https://www.youtube.com/embed/11kr3__SVIs"
+              class="fancybox"
+            >
+              <figure>
+                <img src={video01} alt="" class="img-responsive" />
+                <div class="over1"></div>
+              </figure>
+            </a>
+          </div>
+
+          <div class="title1 left">
+            . <span>our team</span> .
+          </div>
+        </div>
+        <div id="staff" class="clearfix">
+          <div class="staff1">
+            <figure>
+              <img src={team01} alt="" class="img-responsive" />
+            </figure>
+            <div class="caption">
+              <div class="txt1">Jackson Nash</div>
+              <div class="txt2">Tax Advice</div>
+              <ul class="social-block clearfix">
+                <li>
+                  <a href="/">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="staff1">
+            <figure>
+              <img src={team02} alt="" class="img-responsive" />
+            </figure>
+            <div class="caption">
+              <div class="txt1">Alex Manning</div>
+              <div class="txt2">CEO-Founder</div>
+              <ul class="social-block clearfix">
+                <li>
+                  <a href="/">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="staff1">
+            <figure>
+              <img src={team03} alt="" class="img-responsive" />
+            </figure>
+            <div class="caption">
+              <div class="txt1">Ollie Schneider</div>
+              <div class="txt2">Business Planner</div>
+              <ul class="social-block clearfix">
+                <li>
+                  <a href="/">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="staff1">
+            <figure>
+              <img src={team04} alt="" class="img-responsive" />
+            </figure>
+            <div class="caption">
+              <div class="txt1">Ann West</div>
+              <div class="txt2">Financer</div>
+              <ul class="social-block clearfix">
+                <li>
+                  <a href="/">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="staff1">
+            <figure>
+              <img src={team05} alt="" class="img-responsive" />
+            </figure>
+            <div class="caption">
+              <div class="txt1">Jackson Nash</div>
+              <div class="txt2">Tax Advice</div>
+              <ul class="social-block clearfix">
+                <li>
+                  <a href="/">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="staff1">
+            <figure>
+              <img src={team06} alt="" class="img-responsive" />
+            </figure>
+            <div class="caption">
+              <div class="txt1">Jackson Nash</div>
+              <div class="txt2">Tax Advice</div>
+              <ul class="social-block clearfix">
+                <li>
+                  <a href="/">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div id="partners">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-md-3">
+                <div class="partner1">
+                  <a href="/">
+                    <img src={partner01} alt="" class="img-fluid" />
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="partner1">
+                  <a href="/">
+                    <img src={partner02} alt="" class="img-fluid" />
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="partner1">
+                  <a href="/">
+                    <img src={partner03} alt="" class="img-fluid" />
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="partner1">
+                  <a href="/">
+                    <img src={partner04} alt="" class="img-fluid" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Videio;
